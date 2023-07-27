1a2dc068c3c3140a58d2bbab4cf456b569d61034
lovepreet67 <getupyou98@gmail.com>
2023-07-27 23:27:14 +0530

    Menu component added

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Navbar from "@/components/index/navbar/Navbar";
 import LandMain from "@/components/index/LandMain";
 import { Toaster } from "react-hot-toast";
-import { useState } from "react";
+import React, { useState } from "react";
 export default function Home() {
   const [left, setLeft] = useState(2);
   return (
